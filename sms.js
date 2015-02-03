64637a1118913ef2bc986f5c0919464e694f4f00
Kent Lai <Kent-Lai@users.noreply.github.com>
2015-02-04 00:22:06 +0800

    need feedback

--- sms.js
+++ sms.js
@@ -1,20 +1,41 @@
-var account = require("./account.js");
-var client = require("twilio")(account.sid, account.auth_token);
+var public_account = require("./account.js");
+var public_client = require("twilio")(public_account.sid, public_account.auth_token);
 
-exports.test = function()
+
+exports.customize = function(private_account, message, exceptions_handler)
+{
+	var private_client = require("twilio")(private_account.sid, private_account.auth_token);
+	private_client.sendMessage(message, excepitons_handler);
+}
+
+exports.sendMessage = function(dst_num, text, media_urls)
 {
 	var msg = {
-		from : account.num,
-		to : "+886989034211",
-		body : "This is a TEST."
+		from : public_account.num,
+		to : dst_num,
+		body : text
 	};
 
-	client.sendMessage(msg, function(err, responseData)
+	if(media_urls)
+	{
+		msg.mediaUrl = media_urls;
+	}
+
+	public_client.sendMessage(msg, function(err, MIR)
 		{
 			if(err)
 			{
 				LOG.warn(err.message);
 			}
+			else
+			{
+				LOG.warn(MIR);
+			}
 		}
 	);
+}
+
+exports.listMessages = function(list_filter, cb)
+{
+	public_client.listMessages(list_filter, cb);
 }
